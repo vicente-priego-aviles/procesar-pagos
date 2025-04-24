e8297aa417295f7fe354864421c3af38b43d3a26
Vicente Prigego Avilés <vicentepriegoaviles@gmail.com>
2025-04-24 21:11:40 +0200

    Proyecto generado con Spring Initializr

---
+++ src/main/java/dev/javacadabra/demo/procesar_pagos/ProcesarPagosApplication.java
@@ -0,0 +1,13 @@
+package dev.javacadabra.demo.procesar_pagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesarPagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesarPagosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/javacadabra/demo/procesar_pagos/ProcesarPagosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.javacadabra.demo.procesar_pagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesarPagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
